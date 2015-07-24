4cf68ca3d7b3c2a2e3ca85b1a9bf436a38e1a95a
Teck <teck@tekleemedia.com>
2015-07-24 11:07:42 -0700

    setting up user management admin

--- client/templates/users.js
+++ client/templates/users.js
@@ -1,9 +1,23 @@
-Meteor.subscribe('users', function() {
-  console.log(Users.find().fetch());
-});
+Meteor.subscribe('users');
 
 Template.users.helpers({
   users: function(){
     return Users.find().fetch();
+  },
+  numOfUsers: function() {
+    return Users.find().fetch().length;
+  }
+});
+
+Template.users.events({
+  "mousedown .delete-user": function(){
+     var message = 'Are you sure you want to remove "' + this.username + '"?';
+     if(confirm(message)){
+       Users.remove(this._id);
+       console.log('Deleted ' + this.username);
+       return true;
+     }else{
+       return false;
+     }
   }
 });
